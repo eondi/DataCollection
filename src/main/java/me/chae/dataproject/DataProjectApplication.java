c896321eb774349e5a8edc49c12dae960cd3537a
ejchae <ejchae@kissco.co.jp>
2024-05-20 13:09:58 +0900

    init

---
+++ src/main/java/me/chae/dataproject/DataProjectApplication.java
@@ -0,0 +1,13 @@
+package me.chae.dataproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProjectApplication.class, args);
+    }
+
+}
